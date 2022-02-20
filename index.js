4127a389462301d584b3e461b6caf8a0fef36659
Aditya Bagaskara <adityabagaskara019@gmail.com>
2022-02-20 23:21:47 +0700

    new update mock-server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var http = require('http');
 
 var oas3Tools = require('oas3-tools');
-let { PORT = 3000 } = process.env;
+var serverPort = 3000;
 
 // swaggerRouter configuration
 var options = {
@@ -17,8 +17,8 @@ var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/open
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(PORT, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', PORT, PORT);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', PORT);
+http.createServer(app).listen(serverPort, function () {
+    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
 
